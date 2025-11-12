82a5721bdbf492eab864e43d28c654554c6144e8
m-q000 <msamalq306@gmail.com>
2025-11-12 20:40:45 +0300

    (minor) edited project urls

--- Land_Price_Estimator/urls.py
+++ Land_Price_Estimator/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('normal/', include('Normal_User_Side.urls', namespace='normal_user')),
     path('scientist/', include('Data_Scientist_Side.urls', namespace='data_scientist')),
-    path('admin/', include('Admin_Side.urls', namespace='admin_side')),
+    path('admins/', include('Admin_Side.urls', namespace='admin_side')),
     path('', include('Users_Handling_App.urls', namespace='users')),
 ]
