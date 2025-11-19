940d7755c400064ca07e9e88ff2dd6f930a9a8b6
m-q000 <msamalq306@gmail.com>
2025-11-19 16:49:11 +0300

    remade the login page + added "go back to dashboard" button functionality

--- Users_Handling_App/views.py
+++ Users_Handling_App/views.py
@@ -17,35 +17,37 @@ def loginPage(request):
         elif user.type == 'admin':
             return redirect('admin_side:home')
         else:
-            messages.error(request, "Invalid account type. Please contact support.")
+            
             logout(request)
-            return redirect('login')
+            return redirect('users:login')
 
     if request.method == 'POST':
         email = request.POST.get('email')
         password = request.POST.get('password')
 
-        try:
-            user = User.objects.get(email=email)
-        except User.DoesNotExist:
-            messages.error(request, 'User does not exist')
-            return render(request, 'Users_Handling_App/login.html')
+        
 
         user = authenticate(request, email=email, password=password)
 
         if user is not None:
             login(request, user)
+                
+            if user.type == 'normal':
+                return redirect('normal_user:home')
+            elif user.type == 'scientist':
+                return redirect('data_scientist:home')
+            elif user.type == 'admin':
+                return redirect('admin_side:home')
+            else:
+                messages.error(request, "Invalid account type. Please contact support.")
+                logout(request)
+                return redirect('users:login')
 
-            
-        if user.type == 'normal_user':
-            return redirect('normal_user:home')
-        elif user.type == 'data_scientist':
-            return redirect('data_scientist:home')
-        elif user.type == 'admin':
-            return redirect('admin_side:home')
+        
             
         else:
             messages.error(request, 'Email or password is incorrect')
+            return render(request, 'Users_Handling_App/login_page.html')
 
     return render(request, 'Users_Handling_App/login_page.html')
 
